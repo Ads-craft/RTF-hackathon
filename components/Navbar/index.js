ba62923d3c51d8cd052d88a198bf36bee9f31597
lawrenceuchenye <lawuche29@gmail.com>
2023-12-07 20:29:13 +0000

    light ahh

---
+++ components/Navbar/index.js
@@ -0,0 +1,7 @@
+import styles from "./index.css";
+
+const index = () => {
+  return <div></div>;
+};
+
+export default index;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '../styles/globals.css'
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
