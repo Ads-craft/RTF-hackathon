4c6584acd3236c1aa6087bbd396522df11ca7841
lawrenceuchenye <lawuche29@gmail.com>
2023-12-08 14:22:03 +0000

    started

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -3,7 +3,21 @@ import styles from "./index.module.css";
 const index = () => {
   return (
     <div className={styles.nav_container_div}>
-      <h1>Adcraft</h1>
+      {/* logo div */}
+      <div>
+        <h1>
+          Ad<span className="text-light-pink">craft</span>
+        </h1>
+      </div>
+
+      {/* link wallet button main container */}
+      {/* link container */}
+
+      <div></div>
+
+      {/* wallet btn container */}
+
+      <div></div>
     </div>
   );
 };
