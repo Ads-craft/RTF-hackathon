fde59158169a437a823af51156fac495f7ef2cbd
lawrenceuchenye <lawuche29@gmail.com>
2023-12-08 18:57:40 +0000

    links and responsivness started

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./index.module.css";
 
 const index = () => {
@@ -5,31 +6,41 @@ const index = () => {
     <div className={styles.nav_container_div}>
       {/* logo div */}
       <div>
-        <h1>
-          Ad<span className="text-light-pink">craft</span>
-        </h1>
+        <Link href="/">
+          <h1>
+            Ad<span className="text-light-pink">craft</span>
+          </h1>
+        </Link>
       </div>
 
       {/* link container */}
       <div className={styles.links_container_div}>
-        <h1>
-          <a href="#">Home</a>
-        </h1>
+        <Link href="/">
+          <h1>
+            <a href="#">Home</a>
+          </h1>
+        </Link>
         <h1>
           <a href="#">Marketplace</a>
         </h1>
         <h1>
           <a href="#">Explore</a>
         </h1>
-        <h1>
-          <a href="#">Signin</a>
-        </h1>
+        <Link href="auth/signin">
+          <h1>
+            <a href="#">Signin</a>
+          </h1>
+        </Link>
       </div>
 
       {/* wallet btn container */}
 
       <div className={styles.btn_container_div}>
         <i className="fa fa-wallet"></i>Connect Wallet
+      </div>
+
+      <div className={styles.ham_parent_container}>
+        <div className={styles.ham}></div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,13 @@ const HomePage = () => {
   return (
     <>
       <div>
-        <div></div><div></div>
+        <div>
+          <p className="text-bold text-gray-50">Hello world</p>
+        </div>
+        <div></div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default HomePage;
