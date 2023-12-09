da10216bff76da316ea03b4cbe023a76cddc2615
lawrenceuchenye <lawuche29@gmail.com>
2023-12-09 12:27:02 +0000

    adde mobile menu animations

--- components/MobileMenu/index.js
+++ components/MobileMenu/index.js
@@ -1,8 +1,19 @@
 import styles from "./index.module.css";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { motion, useAnimationControls } from "framer-motion";
 
 const index = ({ isTapped }) => {
+  const linkVariant = {
+    hidden: {
+      opacity: 0,
+      x: "-100%",
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+    },
+  };
+
   return (
     <div
       className={
@@ -12,31 +23,86 @@ const index = ({ isTapped }) => {
       }
     >
       <div className={styles.mobile_container}>
-        {/* Link container */}
         <div className={styles.mobile_links_container_div}>
-          <Link style={{ textDecoration: "none" }} href="/">
-            <h1>
-              <a href="#">home</a>
-            </h1>
-          </Link>
-          <h1>
-            <a href="#">marketplace</a>
-          </h1>
-          <h1>
-            <a href="#">explore</a>
-          </h1>
-          <Link style={{ textDecoration: "none" }} href="auth/signin">
-            <h1>
-              <a href="#">signin</a>
-            </h1>
-          </Link>
+          {isTapped && (
+            <Link style={{ textDecoration: "none" }} href="/">
+              <motion.h1
+                initial="hidden"
+                animate="visible"
+                variants={linkVariant}
+                transition={{
+                  delay: 0.05,
+                  type: "spring",
+                }}
+              >
+                <a href="#">home</a>
+              </motion.h1>
+            </Link>
+          )}
+
+          {isTapped && (
+            <motion.h1
+              initial="hidden"
+              animate="visible"
+              variants={linkVariant}
+              transition={{
+                delay: 0.15,
+                type: "spring",
+              }}
+            >
+              <a href="#">marketplace</a>
+            </motion.h1>
+          )}
+
+          {isTapped && (
+            <motion.h1
+              initial="hidden"
+              animate="visible"
+              variants={linkVariant}
+              transition={{
+                delay: 0.25,
+                type: "spring",
+              }}
+            >
+              <a href="#">explore</a>
+            </motion.h1>
+          )}
+
+          {isTapped && (
+            <Link style={{ textDecoration: "none" }} href="auth/signin">
+              <motion.h1
+                initial="hidden"
+                animate="visible"
+                variants={linkVariant}
+                transition={{
+                  delay: 0.35,
+                  type: "spring",
+                }}
+              >
+                <a href="#">signin</a>
+              </motion.h1>
+            </Link>
+          )}
         </div>
 
         {/* wallet btn container */}
 
-        <div className={styles.btn_container_div}>
-          <i className="fa fa-wallet"></i>connect wallet
-        </div>
+        {isTapped && (
+          <motion.div
+            style={{ position: "relative", top: "50%", left: "0px" }}
+            whileTap={{ scale: 1.1 }}
+            initial={{ opacity: 0, y: "40px" }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              transition: { delay: 1.2, type: "spring" },
+            }}
+          >
+            <div className={styles.btn_container_div}>
+              <i className="fa fa-wallet"></i>connect wallet
+            </div>
+          </motion.div>
+        )}
       </div>
     </div>
   );
